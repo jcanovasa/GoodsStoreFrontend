8e3049efef3b4d53f28d30df1a68d71f13299f60
jcanovasa <jaimecanovas.a@gmail.com>
2022-11-18 17:54:00 +0100

    frontend

---
+++ src/app/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  //URL del listado de todos los usuarios del backend
+  private baseURL = "http://localhost:8080/api/users/users";
+
+  constructor(private httpClient : HttpClient) { }
+
+  //Con este método obtengo los usuarios
+  getUserList():Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.baseURL}`)
+  }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+
+    id:number;
+    name:string;
+    surname:string;
+    email:string;
+    pwd:string;
+    birth:string;
+    
+}
